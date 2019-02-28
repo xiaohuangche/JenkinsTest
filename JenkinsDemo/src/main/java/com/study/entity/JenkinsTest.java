bb6a5e273e349bcd2056e6d49d7542f830a7ff0e
laidongxin <L65699192#>
2019-02-28 09:56:23 +0800

    jenkins test

---
+++ JenkinsDemo/src/main/java/com/study/entity/JenkinsTest.java
@@ -0,0 +1,15 @@
+package com.study.entity;
+
+public class JenkinsTest {
+	private String name = "jenkins";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
